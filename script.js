bdc9c35fec1e4b0e28b658058a54d04078ef95ce
Keerthi Nandan <keerthi.nandan@inxsasia.com>
2021-04-27 18:43:25 +0530

    new

---
+++ script.js
@@ -0,0 +1,51 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+var intervalId = setInterval(function() {
+	slideIndex = slideIndex+1;
+  showSlides(slideIndex);
+  if(slideIndex == 4){
+	  slideIn = 1;
+  }
+}, 4000);
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+	var button = document.getElementById( 'topnav' ).getElementsByTagName( 'div' )[0],
+	    menu   = document.getElementById( 'topnav' ).getElementsByTagName( 'ul' )[0];
+
+	
+
+	button.onclick = function() {
+		if ( -1 == menu.className.indexOf( 'srt-menu' ) )
+			menu.className = 'srt-menu';
+
+		if ( -1 != button.className.indexOf( 'toggled-on' ) ) {
+			button.className = button.className.replace( ' toggled-on', '' );
+			menu.className = menu.className.replace( ' toggled-on', '' );
+		} else {
+			button.className += ' toggled-on';
+			menu.className += ' toggled-on';
+		}
+	};
+
